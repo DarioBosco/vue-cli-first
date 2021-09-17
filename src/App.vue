1ecdfd7af8aee30ee223cf208ec16aaacabea8ba
DarioBosco <dario_bosco@protonmail.com>
2021-09-17 17:06:03 +0200

    Jumbotron completato

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<Title msg="La mia prima app con Vue CLI!" />
-		<Hero msg="Wow, boolean.careers so good." />
+		<Hero />
 	</div>
 </template>
 
