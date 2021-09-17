79c7c1f50e884d9f473814eb3001252e385ba145
DarioBosco <dario_bosco@protonmail.com>
2021-09-17 17:05:38 +0200

    Aggiunta del Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<Title msg="La mia prima app con Vue CLI!" />
+		<Hero msg="Wow, boolean.careers so good." />
 	</div>
 </template>
 
 <script>
 import Title from './components/Title.vue';
+import Hero from './components/Hero';
 
 export default {
 	name: 'App',
 	components: {
 		Title,
+		Hero,
 	},
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="image-container">
+		<img src="../assets/Jumbotron.jpg" alt="" />
+		<h2>{{ msg }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Hero',
+	props: {
+		msg: String,
+	},
+};
+</script>
+
+<style scoped>
+.image-container {
+	width: 60%;
+	margin: 0 auto;
+	height: 300px;
+}
+
+.image-container img {
+	width: 100%;
+}
+</style>
